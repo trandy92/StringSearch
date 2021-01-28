58e6aae88f7cca555e33fb379769cd5d3d725eb1
andreas.reichel <andreas.reichel@sovanta.com>
2021-01-28 21:35:58 +0100

    implement of AllPossibleWordsGenerator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+
+
+        String[] alphabet= new String[] {"a", "b", "c", "d"};
+        Vector<String> input = AllPossibleWordsGenerator.getAllPossibleWords(4, alphabet);
+        for (String item : input)
+        {
+            System.out.println(item);
+        }
+
+        Vector<String> results = findSubstring("he", input);
+        for (String item : results)
+        {
+            System.out.println(item + "\n");
+        }
     }
+
+    private static Vector<String> findSubstring(String substring, Vector<String> input) {
+        Vector<String> results = new Vector<String>();
+        for (String item : input)
+        {
+            if(item.startsWith(substring))
+            {
+                results.add(item);
+            }
+        }
+        return results;
+    }
+
+
 }
