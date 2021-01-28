75fe7432cd660820539c9534592101ccaed82db9
andreas.reichel <andreas.reichel@sovanta.com>
2021-01-28 21:55:15 +0100

    refactored of AllPossibleWordsGenerator

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,13 @@ public class Main {
     public static void main(String[] args) {
 
 
+        //String[] alphabet= new String[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t","u", "v","w","x","y","z"};
         String[] alphabet= new String[] {"a", "b", "c", "d"};
-        Vector<String> input = AllPossibleWordsGenerator.getAllPossibleWords(4, alphabet);
-        for (String item : input)
-        {
-            System.out.println(item);
-        }
 
-        Vector<String> results = findSubstring("he", input);
+        Vector<String> input = AllPossibleWordsGenerator.getAllPossibleWords(4, alphabet);
+        System.out.println(input.size());
+
+        Vector<String> results = findSubstring("ab", input);
         for (String item : results)
         {
             System.out.println(item + "\n");
