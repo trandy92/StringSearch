6e3b895bf02e65d1f7d4136b6b560706f21391ef
andreas.reichel <andreas.reichel@sovanta.com>
2021-02-01 18:54:43 +0100

    added rabin karp algorithm for testing purposes

--- src/StringSearch/SearchAlgorithm.java
+++ src/StringSearch/SearchAlgorithm.java
@@ -44,7 +44,8 @@ public class SearchAlgorithm implements Runnable {
                 wordsContainingSubstring.add(item);
             }
         }*/
-        naiveSearch(wordsToSearchThroughCharArray, wordsToSearchThroughCharArray.length, searchString.toCharArray(), searchString.length() );
+        searchRabinKarp(searchString.toCharArray(), searchString.length(), wordsToSearchThroughCharArray, wordsToSearchThroughCharArray.length, 101);
+        //naiveSearch(wordsToSearchThroughCharArray, wordsToSearchThroughCharArray.length, searchString.toCharArray(), searchString.length() );
     }
 
     private void naiveSearch(char[] text, int n, char[] pat, int m  )
@@ -55,7 +56,6 @@ public class SearchAlgorithm implements Runnable {
 
         for(i=0; i < lim; i++)
         {
-
             k=i;
             for(j=0; j<m && text[k]==pat[j];j++) {
                 wordFound.append(text[k]);
@@ -68,21 +68,72 @@ public class SearchAlgorithm implements Runnable {
                     wordFound.append(text[k]);
                     k++;
                 }
-
                 wordsContainingSubstring.add(wordFound.toString());
-
             }else
             {
-                while(i+1 < lim && text[i+1]!= ' ')
+                while(i < lim && text[i]!= ' ')
                 {
                     i++;
                 }
-                i++;
+
             }
             wordFound.setLength(0);
         }
 
     }
+    public final static int d = 26;
+    static void searchRabinKarp(char[] pat, int M, char[] txt, int N, int q)
+    {
+        int i, j;
+        int p = 0; // hash value for pattern
+        int t = 0; // hash value for txt
+        int h = 1;
+
+        // The value of h would be "pow(d, M-1)%q"
+        for (i = 0; i < M - 1; i++)
+            h = (h * d) % q;
+
+        // Calculate the hash value of pattern and first
+        // window of text
+        for (i = 0; i < M; i++) {
+            p = (d * p + pat[i]) % q;
+            t = (d * t + txt[i]) % q;
+        }
+
+        // Slide the pattern over text one by one
+        for (i = 0; i <= N - M; i++) {
+
+            // Check the hash values of current window of text
+            // and pattern. If the hash values match then only
+            // check for characters one by one
+            if (p == t) {
+                /* Check for characters one by one */
+                for (j = 0; j < M; j++) {
+                    if (txt[i+j] != pat[j])
+                        break;
+                }
+
+                // if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1]
+                if (j == M)
+                {
+                    //System.out.println("Pattern found at index " + i);
+                }
+
+            }
+
+            // Calculate hash value for next window of text: Remove
+            // leading digit, add trailing digit
+            if (i < N - M) {
+                t = (d * (t - txt[i] * h) + txt[i+M]) % q;
+
+                // We might get negative value of t, converting it
+                // to positive
+                if (t < 0)
+                    t = (t + q);
+            }
+        }
+    }
+
     public void start () {;
         if (t == null) {
             t = new Thread (this, "newThread");
